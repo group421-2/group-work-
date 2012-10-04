1c74777f1b2d3c0bed4139fb307001b4d91ee54f
Victor <churilovvictor@mail.ru>
2012-10-04 20:55:36 +0700

    ещё 2 кнопки
    !

--- PCI_lab_3/PCI_lab_3/Form1.Designer.cs
+++ PCI_lab_3/PCI_lab_3/Form1.Designer.cs
@@ -43,6 +43,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
@@ -66,17 +69,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 150);
+            this.textBox1.Location = new System.Drawing.Point(54, 197);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "2";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(331, 150);
+            this.textBox2.Location = new System.Drawing.Point(353, 197);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "3";
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
             // 
             // statusStrip1
             // 
@@ -99,26 +106,31 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "function1",
-            "function2",
-            "function3",
-            "function4"});
-            this.comboBox1.Location = new System.Drawing.Point(195, 66);
+            "x-sqrt(9+x)+x^2-4 = 0",
+            "0.1*x^2-x*lnx =0",
+            "x^4-26*x^3+131*x^2-226*x+120=0",
+            "x^4-0.486*x^3-5.792*x^2+0.486*x+4.792=0",
+            "0.1*sinx+3*x-1=0"});
+            this.comboBox1.Location = new System.Drawing.Point(54, 79);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(195, 228);
+            this.trackBar1.Location = new System.Drawing.Point(195, 197);
+            this.trackBar1.Maximum = 1;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(104, 45);
             this.trackBar1.TabIndex = 6;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(235, 25);
+            this.label1.Location = new System.Drawing.Point(51, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 7;
@@ -127,7 +139,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(120, 117);
+            this.label2.Location = new System.Drawing.Point(51, 170);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 8;
@@ -136,7 +148,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(328, 117);
+            this.label3.Location = new System.Drawing.Point(398, 170);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 9;
@@ -145,7 +157,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(224, 182);
+            this.label4.Location = new System.Drawing.Point(224, 170);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 13);
             this.label4.TabIndex = 10;
@@ -159,15 +171,17 @@
             this.button2.TabIndex = 11;
             this.button2.Text = "New_function_add";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(453, 228);
+            this.button3.Location = new System.Drawing.Point(443, 267);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 12;
             this.button3.Text = "set graph";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // checkBox1
             // 
@@ -178,12 +192,47 @@
             this.checkBox1.TabIndex = 13;
             this.checkBox1.Text = "Reset Default";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(368, 60);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(117, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Метод Дихотомии";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(368, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Метод Хорд";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 580);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -226,6 +275,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
--- PCI_lab_3/PCI_lab_3/Form1.cs
+++ PCI_lab_3/PCI_lab_3/Form1.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
 namespace PCI_lab_3
 {
     public partial class Form1 : Form
@@ -15,14 +14,147 @@ namespace PCI_lab_3
         {
             InitializeComponent();
         }
+        private void interval_value()
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                {
+                    textBox1.Text = "2";
+                    textBox2.Text = "3";
+                    break;
+                }
+                case 1:
+                {
+                    textBox1.Text = "1";
+                    textBox2.Text = "2";
+                    break;
+                }
+                case 2:
+                {
+                    textBox1.Text = "19,5";
+                    textBox2.Text = "21,2";
+                    break;
+                }
+                case 3:
+                {
+                    textBox1.Text = "2";
+                    textBox2.Text = "3";
+                    break;
+                }
+                case 4:
+                {
+                    textBox1.Text = "0,8";
+                    textBox2.Text = "1";
+                    break;
+                }
+            }
+        }
+
+        private double f(double x)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                {
+                    return x - Math.Sqrt(9 + x) + x * x - 4;
+                }
+                case 1:
+                {
+                    return 0.1 * x * x - x * Math.Log(x);
+                }
+                case 2:
+                {
+                    return Math.Pow(x, 4) - 26 * Math.Pow(x, 3) + 131 * x * x - 226 * x + 120;
+                }
+                case 3:
+                {
+                    return Math.Pow(x, 4) - 0.486 * Math.Pow(x, 3) - 5.792 * x * x - 0.486 * x + 4.792;
+                }
+                case 4:
+                {
+                    return 0.1 * Math.Sin(x) + 3 * x - 1;
+                }
+                default :
+                return 0;
+                
+            }
+        }
+
+        private double dix()
+        {
+            double a, b, c, eps;
+            int n = 0;
+
+            eps = trackBar1.Value / 1000.0;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+           
+            while(Math.Abs(b-a)>eps)
+            {
+                c = (a + b) / 2;   
+           
+                if ( f(b) * f(c) < 0)
+
+                    a = c;        
+        
+                else 
+
+                    b = c;
+                    
+                n++;
+            }
+            return (a + b) / 2; 
+
+        }
+
+        private double hord()
+        {
+            double a, b, eps;
+            int n = 0;
+
+            eps = trackBar1.Value / 1000.0;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+
+            while (Math.Abs(b - a) > eps)
+            {
+                a = b - (b - a) * f(b) / (f(b) - f(a));
+                b = a - (a - b) * f(a) / (f(a) - f(b));
+                n++;
+            }
+
+            return b;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (f(Convert.ToDouble(textBox1.Text)) * f(Convert.ToDouble(textBox2.Text)) < 0)
+            {
+                if (radioButton1.Checked == true)
 
+                    label5.Text = Convert.ToString(dix());
+
+                else
+
+                    label5.Text = Convert.ToString(hord());
+            }
+            else
+                MessageBox.Show("Вы ввели интервал на котором нет корней", "Некорректный ввод данных", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            if (checkBox1.Checked == true)
+
+                this.Form1_Load(this, e);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 100;
+            trackBar1.Value = 100;
+            comboBox1.SelectedIndex = 0;
+            radioButton1.Checked = true;
+            label5.Text = "";
 
         }
 
@@ -30,5 +162,52 @@ namespace PCI_lab_3
         {
             
         }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(char.IsControl(e.KeyChar)))
+                if (!((e.KeyChar.ToString() == ",") && (textBox1.Text.IndexOf(",") == -1  )))
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(char.IsControl(e.KeyChar)))
+                if (!((e.KeyChar.ToString() == ",") && (textBox1.Text.IndexOf(",") == -1)))
+                    e.Handled = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            interval_value();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label4.Text = Convert.ToString(trackBar1.Value / 1000.0);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.Show();
+        }
+
     }
 }
