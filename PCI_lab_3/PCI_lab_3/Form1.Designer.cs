11efa39c7fb1c729dc95f350d55e6b868cfa9392
PeoneEr <peoneerko@gmail.com>
2012-10-04 19:05:10 +0700

    Основы графиков

--- PCI_lab_3/PCI_lab_3/Form1.Designer.cs
+++ PCI_lab_3/PCI_lab_3/Form1.Designer.cs
@@ -28,34 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 63);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Say hello";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(151, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(168, 150);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(930, 661);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Пиздатые графики";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+
     }
 }
 
--- PCI_lab_3/PCI_lab_3/Form1.cs
+++ PCI_lab_3/PCI_lab_3/Form1.cs
@@ -16,9 +16,38 @@ namespace PCI_lab_3
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            MessageBox.Show("Hello world!");
+            Pen grid = new Pen(new SolidBrush(Color.Black), 1f); // Рисую ось черными линиями.
+            Graphics graphics = panel1.CreateGraphics(); // Рисую график на Panel.
+
+            // Рисую ось:
+            graphics.DrawLine(grid, 0, panel1.Width / 2, panel1.Width, panel1.Width / 2);
+            graphics.DrawLine(grid, panel1.Width / 2, 0, panel1.Width / 2, panel1.Height);
+            int scale = 50;
+            int x = panel1.Width;
+            /**
+             * Рисуем ось Х
+             */
+            while (x > 0)
+            {
+                graphics.DrawLine(grid, x, panel1.Width / 2 - 5, x, panel1.Width / 2 + 5);
+                x -= scale;
+            }
+            graphics.DrawLine(grid, 
+
+            /**
+             * Рисуем ось Y
+             */
+            int y = panel1.Height;
+            while (y > 0)
+            {
+                graphics.DrawLine(grid, panel1.Height / 2 - 5, y, panel1.Width / 2 + 5, y);
+                y -= scale;
+            }
+
         }
     }
+
+        
 }
