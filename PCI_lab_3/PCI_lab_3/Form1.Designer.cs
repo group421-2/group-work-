821ce313605c51529c08752ed39913dadf40fb04
PeoneEr <peoneerko@gmail.com>
2012-10-04 20:17:59 +0700

    еботня продолжается

--- PCI_lab_3/PCI_lab_3/Form1.Designer.cs
+++ PCI_lab_3/PCI_lab_3/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -40,22 +42,35 @@
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(151, 618);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(600, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(930, 661);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Пиздатые графики";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
 
     }
 }
--- PCI_lab_3/PCI_lab_3/Form1.cs
+++ PCI_lab_3/PCI_lab_3/Form1.cs
@@ -16,72 +16,82 @@ namespace PCI_lab_3
             InitializeComponent();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void drawGraphic(float scale)
         {
+
             Pen grid = new Pen(new SolidBrush(Color.Black), 1f); // Рисую ось черными линиями.
             Pen arrows = new Pen(new SolidBrush(Color.Black), 2f);
             Graphics graphics = panel1.CreateGraphics(); // Рисую график на Panel.
+            graphics.Clear(panel1.BackColor);
+
 
             // Рисую ось:
             graphics.DrawLine(grid, 0, panel1.Width / 2, panel1.Width, panel1.Width / 2);
             graphics.DrawLine(grid, panel1.Width / 2, 0, panel1.Width / 2, panel1.Height);
-            int scale = 50;
-            int x = panel1.Width;
-            /**
-             * Размечаем ось Х
-             */
-            while (x > 0)
-            {
-                graphics.DrawLine(grid, x, panel1.Width / 2 - 5, x, panel1.Width / 2 + 5);
-                x -= scale;
-            }
+
             /**
              * Рисование стрелки на оси Х
              */
-            graphics.DrawLine(arrows, panel1.Width, panel1.Height / 2, panel1.Width - 15, panel1.Height /2 - 10);
+            graphics.DrawLine(arrows, panel1.Width, panel1.Height / 2, panel1.Width - 15, panel1.Height / 2 - 10);
             graphics.DrawLine(arrows, panel1.Width, panel1.Height / 2, panel1.Width - 15, panel1.Height / 2 + 10);
 
 
-            /**
-             * Размечаем ось Y
-             */
-            int y = panel1.Height;
-            while (y > 0)
-            {
-                graphics.DrawLine(grid, panel1.Height / 2 - 5, y, panel1.Width / 2 + 5, y);
-                y -= scale;
-            }
-            
             /**
              * Рисование стрелки на оси У
              */
             graphics.DrawLine(arrows, panel1.Width / 2, 0, panel1.Height / 2 - 15, 15);
             graphics.DrawLine(arrows, panel1.Width / 2, 0, panel1.Height / 2 + 15, 15);
             drawGraphic(scale);
-            
-        }
-
-        private void drawGraphic(float scale)
-        {
-            float i = 5;
-            float x1,y1,
+            double i = panel1.Width / 2 / scale;
+            double x1, y1,
                  x2, y2;
             Pen colorGraphic = new Pen(new SolidBrush(Color.Red), 1f);
-            Graphics graphics = panel1.CreateGraphics(); // Рисую график на Panel.
-            float mark = (panel1.Width / 2) / scale;
-            while (i > -5)
+            double mark = (panel1.Width / 2) / scale;
+            int pointX = panel1.Width, pointY = panel1.Height;
+
+            /**
+             * Размечаем ось Х
+            */
+            while (pointX > 0)
+            {
+                graphics.DrawLine(grid, pointX, panel1.Width / 2 - 5, pointX, panel1.Width / 2 + 5);
+                pointX -= Convert.ToInt32(scale);
+            }
+            /**
+             * Размечаем ось Y
+             */
+            while (pointY > 0)
+            {
+                graphics.DrawLine(grid, panel1.Height / 2 - 5, pointY, panel1.Width / 2 + 5, pointY);
+                pointY -= Convert.ToInt32(scale);
+            }
+            while (i > -panel1.Width / 2 / scale)
             {
                 x1 = i;
-                y1 = x1 * x1 * x1;
+                y1 = Math.Sin(x1);
                 x1 = panel1.Width - (mark - x1) * scale;
                 y1 = (panel1.Height / 2) - y1 * scale;
                 i--;
                 x2 = i;
-                y2 = x2 * x2 * x2;
+                y2 = Math.Sin(x2);
                 x2 = panel1.Width - (mark - x2) * scale;
                 y2 = (panel1.Height / 2) - y2 * scale;
-                graphics.DrawLine(colorGraphic, x1, y1, x2, y2);
+                graphics.DrawLine(colorGraphic, (float)x1, (float)y1, (float)x2, (float)y2);
             }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = 5;
+            trackBar1.Maximum = 50;
+            trackBar1.TickFrequency = 5;
+            float scale = trackBar1.Value;
+            drawGraphic(scale);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            drawGraphic(trackBar1.Value);
         }
     }
 
