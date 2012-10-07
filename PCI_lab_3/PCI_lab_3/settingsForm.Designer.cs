7a031677a7b086c0e07debe6dcec5894f76a0d9c
PeoneEr <peoneerko@gmail.com>
2012-10-07 20:07:22 +0700

    хуйняяяяя

---
+++ PCI_lab_3/PCI_lab_3/settingsForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PCI_lab_3
+{
+    partial class settingsForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.graphiColor = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.graphiColor);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 238);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки графика";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Цвет графика";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Толщина линии";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(21, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // graphiColor
+            // 
+            this.graphiColor.Location = new System.Drawing.Point(21, 45);
+            this.graphiColor.Name = "graphiColor";
+            this.graphiColor.Size = new System.Drawing.Size(67, 32);
+            this.graphiColor.TabIndex = 4;
+            this.graphiColor.Click += new System.EventHandler(this.panel1_Click);
+            this.graphiColor.Paint += new System.Windows.Forms.PaintEventHandler(this.graphiColor_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(141, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(165, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Сетка";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // settingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "settingsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "settingsForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel graphiColor;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PCI_lab_3/PCI_lab_3/settingsForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PCI_lab_3
+{
+    public partial class settingsForm : Form
+    {
+        public static Color graphicColor; //цвет графика
+        public static Single thickness; //толщина графика
+        public static bool net; //сетка
+
+        public settingsForm()
+        {
+            InitializeComponent();
+            parseSettings("C:\\Sites\\group-work-\\PCI_lab_3\\PCI_lab_3\\settings.ini");
+            comboBox1.SelectedIndex = (int)thickness - 1;
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void parseSettings(string settings)
+        {
+            byte i = 1;
+            string s = "";
+            if (!File.Exists(settings))
+            {
+                MessageBox.Show("Идите нахуй");
+            }
+            StreamReader sr = File.OpenText(settings);
+            while ((s = sr.ReadLine()) != null)
+            {
+                switch (i)
+                {
+                    case 1:
+                        int RGB = int.Parse(s, System.Globalization.NumberStyles.AllowHexSpecifier);
+                        graphicColor = Color.FromArgb(RGB); i++;
+                        break;
+                    case 2: thickness = Single.Parse(s); i++;
+                        break;
+                    case 3: net = bool.Parse(s); i = 1;
+                        break;
+                }
+            }
+            sr.Close();
+        }
+
+        private void graphiColor_Paint(object sender, PaintEventArgs e)
+        {
+            graphiColor.BackColor = graphicColor;
+        }
+    }
+}
