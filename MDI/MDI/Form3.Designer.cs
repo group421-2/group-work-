037f649182cfaafcdb18d73b241970491f134fed
PeoneEr <peoneerko@gmail.com>
2012-10-23 07:36:54 +0700

    new file:   MDI/MDI/bin/Debug/openFileDialog2

---
+++ MDI/MDI/Form3.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MDI
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(477, 461);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(265, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 1;
+            this.panel1.Visible = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form3";
+            this.Text = "Безымянный";
+            this.Resize += new System.EventHandler(this.Form3_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+
+    }
+}
---
+++ MDI/MDI/Form3.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            drawing = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            cl = Color.Black;
+        }
+
+        private Bitmap drawing;
+        public Color cl = new Color();
+        private bool leftDown = false;
+        private int prevX;
+        private int prevY;
+        private int currentX;
+        private int currentY;
+        private Boolean k = true;
+
+        public void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cl = panel1.BackColor;
+            if (leftDown)
+            {
+                Graphics g = Graphics.FromImage(drawing);
+
+                currentX = e.X;
+                currentY = e.Y;
+
+                if (k == true)
+                {
+                    g.DrawLine(new Pen(cl, 5), new Point(currentX, currentY), new Point(prevX, prevY));
+                }
+                else
+                {
+                    g.FillRectangle(new SolidBrush(pictureBox1.BackColor), currentX, currentY, 5 * 5, 5 * 5);
+                }
+
+                pictureBox1.Invalidate();
+
+                prevX = currentX;
+                prevY = currentY;
+                g.Dispose();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            leftDown = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                leftDown = true;
+
+                prevX = e.X;
+                prevY = e.Y;
+            }
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            leftDown = false;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(drawing, 0, 0);
+        }
+
+        private void Form3_Resize(object sender, EventArgs e)
+        {
+            drawing = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+    }
+}
